df088b91fd1a5a1ba1f640e2c653a5a5fda0ab18
Zoe <zmb6893@g.rit.edu>
2023-03-05 00:21:12 -0500

    Started on csv

--- inventori/src/app/app.component.ts
+++ inventori/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Item } from './inventori-table/inventori-table.component';
+const fs = require("fs");
+const { stringify } = require("csv-stringify");
+const db = require("./db");
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventori';
+  productInfoList: Item[] = [];
+
+  ngOnInit = () => {
+    // Load the csv info from the file.
+    const filename = "inventori/src/assets/inventoryInfo.csv"
+    const writableStream = fs.createWriteStream(filename);
+    const columns = [
+      "productName",
+      "initialQuantity",
+      "currentQuantity",
+      "quantityType"
+    ];
+    
+  }
 }
