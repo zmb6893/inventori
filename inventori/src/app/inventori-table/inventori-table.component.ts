d1dd15c2a72e032d1a5f819e98c498f79e5fe16b
brainuser5705 <codeuser5705@outlook.com>
2023-03-05 03:46:38 -0500

    buggy angular

--- inventori/src/app/inventori-table/inventori-table.component.ts
+++ inventori/src/app/inventori-table/inventori-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ItemCSV } from '../app.component';
 
 export interface Item {
   quantity: number;
@@ -7,11 +8,11 @@ export interface Item {
   status: number;
 }
 
-const inventory: Item[] = [
-  {productName: 'Apples', quantity: 15, quantityType: 'Whole Number', status: 1},
-  {productName: 'Oranges', quantity: 12, quantityType: 'Whole Number', status: .5},
-  {productName: 'Flour', quantity: 130, quantityType: 'g', status: .25},
-  {productName: 'Potatoes', quantity: 3, quantityType: 'Whole Number', status: .75},
+var inventory: Item[] = [
+  // {productName: 'Apples', quantity: 15, quantityType: 'Whole Number', status: 1},
+  // {productName: 'Oranges', quantity: 12, quantityType: 'Whole Number', status: .5},
+  // {productName: 'Flour', quantity: 130, quantityType: 'g', status: .25},
+  // {productName: 'Potatoes', quantity: 3, quantityType: 'Whole Number', status: .75},
 ];
 
 
@@ -22,15 +23,45 @@ const inventory: Item[] = [
   styleUrls: ['./inventori-table.component.css']
 })
 export class InventoriTableComponent {
+  @Input() products: ItemCSV[] = [];
+
   displayedColumns: string[] = ['productName', 'quantity', 'quantityType', 'status', 'add', 'use'];
-  dataSource = inventory;
+  dataSource : Item[] = inventory;
+
+  constructor(){
+    this.ngOnInit();
+  }
+  
+  ngOnLoad = () => {
+    
+  }
+
+
+  ngOnInit(){
+    console.log("array: " + this.products);
+  
+    this.products.forEach(i => {
+      this.dataSource.push({
+        productName: i.productName, 
+        quantity: i.currentQuantity, 
+        quantityType: i.quantityType, 
+        status: i.currentQuantity / i.initialQuantity,
+      });
+      // console.log(i);
+    })
+    // this.dataSource = this.convertItemCSV();
+  }
+
+  
 
   openAddModal = (item: any) => {
+    console.log(this.products);
     console.log(`Adding to the item ${item.productName}`);
   };
 
   openUseModal = (item: any) => {
     console.log(`Adding to the item ${item.productName}`);
   };
+
 }
 
