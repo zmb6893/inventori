52249952d1e9e46f950a046f172e50dd9fd833e4
brainuser5705 <codeuser5705@outlook.com>
2023-03-05 04:19:16 -0500

    working button

--- server/csv.js
+++ server/csv.js
@@ -8,35 +8,6 @@ function getAllRows(){
     return csvToJson.fieldDelimiter(',').getJsonFromCsv(filename);
 }
 
-function addRow(data){
-    // productName, initialQuantity, type, limit
-    // const writableStream = fs.createWriteStream(".");
-
-    // const columns = [
-    //     "productName",
-    //     "initialQuantity",
-    //     "currentQuantity",
-    //     "quantityType",
-    //     "limit"
-    // ];
-
-    // const stringifer = stringify({header: false, columns: columns});
-    // create a Buffer from data
-    stringify(someData, {
-        header: false
-    }, function (err, output) {
-        fs.writeFile(filename, output);
-    });
-
-    
-}
-
-
-// fs.createReadStream("../data.csv")
-    // .pipe(parse({ delimiter: ",", from_line: 2}))
-    // .on("data", function(row){
-        
-    // });
 console.log(getAllRows());
 
 module.exports = { getAllRows };
