4735e2eb20de8b37769469692d28460c0b2b4fa5
Zoe <zmb6893@g.rit.edu>
2023-03-04 19:53:21 -0500

    Added in a weight card

---
+++ inventori/src/app/weight-confirmation/weight-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weight-confirmation',
+  templateUrl: './weight-confirmation.component.html',
+  styleUrls: ['./weight-confirmation.component.css']
+})
+export class WeightConfirmationComponent {
+  weight: number = 0;
+}
